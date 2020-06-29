5da4844bef88ec352c81ac72b44ee003a1359627
Kamal <kamalgupta308@gmail.com>
2020-06-29 13:02:10 -0400

    added brew and iterm config

--- install.sh
+++ install.sh
@@ -13,3 +13,12 @@ git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive
 git submodule update --init --recursive "${DOTBOT_DIR}"
 
 "${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
+
+# brew
+brew bundle
+
+# iTerm2
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
